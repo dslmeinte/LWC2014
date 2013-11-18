280f034a73f2b97dec4b1b30640d1251cb7abc28
Meinte Boersma <meinte.boersma@gmail.com>
2013-11-18 16:27:02 +0100

    work-in-progress: moved to widget-based implementation - not yet fully reactive

--- reference-implementation/web/js/widgets.js
+++
@@ -1,6 +0,0 @@
-function mkSimpleFormElement(id, label, inputElt) {
-	var divElt = $('<div>').addClass('simpleFormElement');
-	var labelElt = $('<label>').attr('for', id);
-	divElt.append(labelElt);
-	divElt.append(inputElt);
-}
