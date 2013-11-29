4700c750c17eeaf24c8ed0c881564628a159dcb3
Meinte Boersma <meinte.boersma@gmail.com>
2013-11-29 14:26:48 +0100

    removed minified versions: no point and only upsets JSHint

--- reference-implementation/scripts/prepare.sh
+++ reference-implementation/scripts/prepare.sh
@@ -2,11 +2,7 @@ cd ..
 ln -s ../README.md .
 
 cd web/js/lib
-curl http://code.jquery.com/jquery-2.0.3.min.js > jquery-2.0.3.min.js
 curl http://code.jquery.com/jquery-2.0.3.js > jquery-2.0.3.js
-curl http://code.jquery.com/jquery-2.0.3.min.map > jquery-2.0.3.min.map
 curl http://underscorejs.org/underscore.js > underscore.js
-curl http://underscorejs.org/underscore-min.js > underscore-min.js
-curl http://underscorejs.org/underscore-min.map > underscore-min.map
 curl https://raw.github.com/BobKnothe/autoNumeric/master/autoNumeric.js > autoNumeric.js
 
