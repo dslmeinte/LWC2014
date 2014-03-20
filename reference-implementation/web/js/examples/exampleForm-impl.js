9335d1ff57d8e65f6a698291bf8336e9234e24d3
delphino <angelohulshout@ieee.org>
2014-03-20 10:12:37 +0100

    Added conditionalFormElement

--- reference-implementation/web/js/examples/exampleForm-impl.js
+++ reference-implementation/web/js/examples/exampleForm-impl.js
@@ -1,33 +1,49 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	/*
-	 * The following code duplicates the structure and details of the example
-	 * model in the assignment's text.
-	 */
-	var form = new QLrt.FormWidget({ name: "Box1HouseOwning", submitCallback: persist});
-	var hasSoldHouse = new QLrt.SimpleFormElementWidget({ name: "hasSoldHouse", label: "Did you sell a house in 2010?", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(form);
-	new QLrt.SimpleFormElementWidget({ name: "hasBoughtHouse", label: "Did you buy a house in 2010?", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(form);
-	new QLrt.SimpleFormElementWidget({ name: "hasMaintLoan", label: "Did you enter a loan for maintenance/reconstruction?", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(form);
+    /*
+     * The following code duplicates the structure and details of the example
+     * model in the assignment's text.
+     */
+    var form = new QLrt.FormWidget({ name: "Box1HouseOwning", submitCallback: persist});
+    var hasSoldHouse = new QLrt.SimpleFormElementWidget({ name: "hasSoldHouse", label: "Did you sell a house in 2010?", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(form);
+    var hasBoughtHouse = new QLrt.SimpleFormElementWidget({ name: "hasBoughtHouse", label: "Did you buy a house in 2010?", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(form);
+    new QLrt.ConditionalFormElementWidget({ name: "buyingPrice", label: "What price was the house bought for:", valueWidget: new QLrt.MoneyValueWidget(), lazyValue: new QLrt.LazyValue(
+        function () {
+            return [ hasBoughtHouse ];
+        },
+        function (hasBoughtHouse) {
+            return hasBoughtHouse;
+        }
+    )}).appendTo(form);
+    new QLrt.SimpleFormElementWidget({ name: "hasMaintLoan", label: "Did you enter a loan for maintenance/reconstruction?", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(form);
 
-	var group1 = (new QLrt.ConditionalGroupWidget(new QLrt.LazyValue(
-			function () { return [ hasSoldHouse ]; },
-			function (hasSoldHouse) { return hasSoldHouse; }
-		))).appendTo(form);
-	var sellingPrice = (new QLrt.SimpleFormElementWidget({ name: "sellingPrice", label: "Price the house was sold for:", valueWidget: new QLrt.MoneyValueWidget() })).appendTo(group1);
-	var privateDebt = (new QLrt.SimpleFormElementWidget({ name: "privateDebt", label: "Private debts for the sold house:", valueWidget: new QLrt.MoneyValueWidget() })).appendTo(group1);
-	(new QLrt.SimpleFormElementWidget({ label: "Value residue:", valueWidget: new QLrt.MoneyValueWidget(new QLrt.LazyValue(
-			function () { return [ sellingPrice, privateDebt ]; },
-			function (sellingPrice, privateDebt) { return sellingPrice - privateDebt; }
-		)) })).appendTo(group1);
+    var group1 = (new QLrt.ConditionalGroupWidget(new QLrt.LazyValue(
+        function () {
+            return [ hasSoldHouse ];
+        },
+        function (hasSoldHouse) {
+            return hasSoldHouse;
+        }
+    ))).appendTo(form);
+    var sellingPrice = (new QLrt.SimpleFormElementWidget({ name: "sellingPrice", label: "Price the house was sold for:", valueWidget: new QLrt.MoneyValueWidget() })).appendTo(group1);
+    var privateDebt = (new QLrt.SimpleFormElementWidget({ name: "privateDebt", label: "Private debts for the sold house:", valueWidget: new QLrt.MoneyValueWidget() })).appendTo(group1);
+    (new QLrt.SimpleFormElementWidget({ label: "Value residue:", valueWidget: new QLrt.MoneyValueWidget(new QLrt.LazyValue(
+        function () {
+            return [ sellingPrice, privateDebt ];
+        },
+        function (sellingPrice, privateDebt) {
+            return sellingPrice - privateDebt;
+        }
+    )) })).appendTo(group1);
 
-	// (The above'd probably look better in CoffeeScript...)
+    // (The above'd probably look better in CoffeeScript...)
 
-	$('#QL-content').append(form.domElement());
-	form.activate();
+    $('#QL-content').append(form.domElement());
+    form.activate();
 
-	function persist (data) {
-		localStorage['QL-persist'] = JSON.stringify(data);
-	}
+    function persist(data) {
+        localStorage['QL-persist'] = JSON.stringify(data);
+    }
 
 });
 
--- reference-implementation/web/js/framework/structural-widgets.js
+++ reference-implementation/web/js/framework/structural-widgets.js
@@ -7,102 +7,112 @@
  */
 QLrt.FormWidget = function (settings) {
 
-	if (typeof (settings) !== 'object'
-			|| settings.name === undefined
-			|| settings.submitCallback === undefined
-	) {
-		throw 'invalid or incomplete settings';
-	}
+    if (typeof (settings) !== 'object'
+        || settings.name === undefined
+        || settings.submitCallback === undefined
+        ) {
+        throw 'invalid or incomplete settings';
+    }
 
-	var outerContainer = QLrt.mk('div', 'formcontainer').hide().append(QLrt.mk('h2').text('Form: ' + settings.name));
-	var innerContainer = QLrt.mk('div', 'form').appendTo(outerContainer);
-	var submitButton = QLrt.mk('button').prop('disabled', true).append('Submit').appendTo(outerContainer).click(function () { settings.submitCallback(asJSON()); });
+    var outerContainer = QLrt.mk('div', 'formcontainer').hide().append(QLrt.mk('h2').text('Form: ' + settings.name));
+    var innerContainer = QLrt.mk('div', 'form').appendTo(outerContainer);
+    var submitButton = QLrt.mk('button').prop('disabled', true).append('Submit').appendTo(outerContainer).click(function () {
+        settings.submitCallback(asJSON());
+    });
 
-	this.domElement = function () {
-		return outerContainer;
-	};
+    this.domElement = function () {
+        return outerContainer;
+    };
 
-	var children = [];
+    var children = [];
 
-	this.append = function (widget) {
-		children.push(widget);
-		innerContainer.append(widget.domElement());
-	};
+    this.append = function (widget) {
+        children.push(widget);
+        innerContainer.append(widget.domElement());
+    };
 
-	var propagatingUpdateLatch = false;
+    var propagatingUpdateLatch = false;
 
-	this.signalChange = function () {
-		if (propagatingUpdateLatch) return;
+    this.signalChange = function () {
+        if (propagatingUpdateLatch) return;
 
-		propagatingUpdateLatch = true;
-		_.each(children, function (subWidget) { subWidget.update(); });
-		propagatingUpdateLatch = false;
+        propagatingUpdateLatch = true;
+        _.each(children, function (subWidget) {
+            subWidget.update();
+        });
+        propagatingUpdateLatch = false;
 
-		submitButton.prop('disabled', !complete());
-	};
+        submitButton.prop('disabled', !complete());
+    };
 
-	this.activate = function () {
-		this.signalChange();			// initial setting of correct visualization
-		outerContainer.show();
-	};
+    this.activate = function () {
+        this.signalChange();			// initial setting of correct visualization
+        outerContainer.show();
+    };
 
-	function complete () {
-		return _.all(children, function (subWidget) { return subWidget.defined(); });
-	}
+    function complete() {
+        return _.all(children, function (subWidget) {
+            return subWidget.defined();
+        });
+    }
 
-	function asJSON () {
-		var result = {};
+    function asJSON() {
+        var result = {};
 
-		_.each(children, function (subWidget) {
-			_.extend(result, subWidget.asJSON());
-		});
+        _.each(children, function (subWidget) {
+            _.extend(result, subWidget.asJSON());
+        });
 
-		return result;
-	}
+        return result;
+    }
 
 };
 
 
 QLrt.ConditionalGroupWidget = function (lazyValue) {
 
-	QLrt.Child.call(this);
+    QLrt.Child.call(this);
 
-	var container = QLrt.mk('div', 'group');
+    var container = QLrt.mk('div', 'group');
 
-	this.domElement = function () {
-		return container;
-	};
+    this.domElement = function () {
+        return container;
+    };
 
-	var children = [];
+    var children = [];
 
-	this.append = function (widget) {
-		children.push(widget);
-		container.append(widget.domElement());
-	};
+    this.append = function (widget) {
+        children.push(widget);
+        container.append(widget.domElement());
+    };
 
-	this.defined = function () {
-		return !lazyValue.evaluate() || _.all(children, function (subWidget) { return subWidget.defined(); });
-	};
+    this.defined = function () {
+        return !lazyValue.evaluate() || _.all(children, function (subWidget) {
+            return subWidget.defined();
+        });
+    };
 
-	this.update = function () {
-		var value = lazyValue.evaluate();
-		container.toggle(value);
-		if (value) {
-			_.each(children, function (subWidget) { subWidget.update(); });
-		}
-	};
+    this.update = function () {
+        var value = lazyValue.evaluate();
+        container.toggle(value);
+        if (value) {
+            _.each(children, function (subWidget) {
+                subWidget.update();
+            });
+        }
+    };
 
-	this.asJSON = function () {
-		var result = {};
+    this.asJSON = function () {
+        var result = {};
 
-		if (lazyValue.evaluate()) {
-			_.each(children, function (subWidget) {
-				_.extend(result, subWidget.asJSON());
-			});
-		}
+        if (lazyValue.evaluate()) {
+            _.each(children, function (subWidget) {
+                _.extend(result, subWidget.asJSON());
+            });
+        }
 
-		return result;
-	};
+        return result;
+    };
 
 };
 QLrt.ConditionalGroupWidget.prototype = Object.create(QLrt.Child.prototype);
@@ -110,51 +120,106 @@ QLrt.ConditionalGroupWidget.prototype = Object.create(QLrt.Child.prototype);
 
 QLrt.SimpleFormElementWidget = function (settings) {
 
-	QLrt.Child.call(this);
+    QLrt.Child.call(this);
 
-	if (typeof (settings) !== 'object'
-			|| settings.label === undefined
-			|| settings.valueWidget === undefined) {
-		throw 'invalid or incomplete settings';
-	}
+    if (typeof (settings) !== 'object'
+        || settings.label === undefined
+        || settings.valueWidget === undefined) {
+        throw 'invalid or incomplete settings';
+    }
 
-	if (settings.valueWidget instanceof QLrt.Child) {
-		settings.valueWidget.appendTo(this);
-	}
+    if (settings.valueWidget instanceof QLrt.Child) {
+        settings.valueWidget.appendTo(this);
+    }
 
-	var outerContainer = QLrt.mk('div', 'simpleFormElement');
-	QLrt.mk('label').appendTo(outerContainer).append(settings.label).append(settings.valueWidget.domElement());
+    var outerContainer = QLrt.mk('div', 'simpleFormElement');
+    QLrt.mk('label').appendTo(outerContainer).append(settings.label).append(settings.valueWidget.domElement());
 
-	this.domElement = function () {
-		return outerContainer;
-	};
+    this.domElement = function () {
+        return outerContainer;
+    };
 
-	this.value = function () {
-		return settings.valueWidget.value();
-	};
+    this.value = function () {
+        return settings.valueWidget.value();
+    };
 
-	this.setValue = function (val) {
-		return settings.valueWidget.setValue(val);
-	};
+    this.setValue = function (val) {
+        return settings.valueWidget.setValue(val);
+    };
 
-	this.defined = function () {
-		return settings.valueWidget.defined();
-	};
+    this.defined = function () {
+        return settings.valueWidget.defined();
+    };
 
-	this.update = function () {
-		settings.valueWidget.update();
-	};
+    this.update = function () {
+        settings.valueWidget.update();
+    };
 
-	this.asJSON = function () {
-		var result = {};
+    this.asJSON = function () {
+        var result = {};
 
-		if (!settings.valueWidget.computed) {
-			result[settings.name] = this.value();
-		}
+        if (!settings.valueWidget.computed) {
+            result[settings.name] = this.value();
+        }
 
-		return result;
-	};
+        return result;
+    };
 
 };
 QLrt.SimpleFormElementWidget.prototype = Object.create(QLrt.Child.prototype);
 
+QLrt.ConditionalFormElementWidget = function (settings) {
+
+    QLrt.Child.call(this);
+
+    if (typeof (settings) !== 'object'
+        || settings.label === undefined
+        || settings.valueWidget === undefined
+        || typeof(settings.lazyValue) !== 'object' ) {
+        throw 'invalid or incomplete settings';
+    }
+
+    if (settings.valueWidget instanceof QLrt.Child) {
+        settings.valueWidget.appendTo(this);
+    }
+
+    var outerContainer = QLrt.mk('div', 'conditionalFormElement');
+    QLrt.mk('label').appendTo(outerContainer).append(settings.label).append(settings.valueWidget.domElement());
+
+    this.domElement = function () {
+        return outerContainer;
+    };
+
+    this.value = function () {
+        return settings.valueWidget.value();
+    };
+
+    this.setValue = function (val) {
+        return settings.valueWidget.setValue(val);
+    };
+
+    this.defined = function () {
+        return settings.valueWidget.defined();
+    };
+
+    this.update = function () {
+        var value = settings.lazyValue.evaluate();
+
+        outerContainer.toggle(value);
+        if (value) {
+            settings.valueWidget.update();
+        }
+    };
+
+    this.asJSON = function () {
+        var result = {};
+
+        if (!settings.valueWidget.computed) {
+            result[settings.name] = this.value();
+        }
+
+        return result;
+    };
+
+};
+QLrt.ConditionalFormElementWidget.prototype = Object.create(QLrt.Child.prototype);
