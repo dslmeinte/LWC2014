758392fd722d41c7ddc4b88098b6d7cbe5da676f
Meinte Boersma <meinte.boersma@gmail.com>
2014-03-27 18:02:27 +0100

    added Enrico's widgets for issue #4

--- reference-implementation/web/js/examples/example-allWidgets.js
+++ reference-implementation/web/js/examples/example-allWidgets.js
@@ -2,44 +2,48 @@ $(document).ready(function() {
 
 	var form = new QLrt.FormWidget({ name: "Widgets gallery", submitCallback: persist});
 
-	var stringValue = new QLrt.SimpleFormElementWidget({ name: "string value", label: "String value:", valueWidget: new QLrt.StringValueWidget()}).appendTo(form);
+	new QLrt.TextWidget({ text: "This form contains an example for each of the available widgets." }).appendTo(form);
+
+	var valueSection = new QLrt.SectionWidget({ label: "Value input" }).appendTo(form);
+	var computedSection = new QLrt.SectionWidget({ label: "Computed values (definedness-check" }).appendTo(form);
+
+	var stringValue = new QLrt.SimpleFormElementWidget({ name: "string value", label: "String value:", valueWidget: new QLrt.StringValueWidget()}).appendTo(valueSection);
 	new QLrt.SimpleFormElementWidget({ label: "string defined?", valueWidget: new QLrt.BooleanValueWidget(
 		new QLrt.LazyValue(function () { return [ stringValue ]; }, function (stringValue) { return stringValue !== undefined; }, true)
-	) }).appendTo(form);
-
+	) }).appendTo(computedSection);
 	var qualityEnum = [ "good", "bad", "don't know" ];
-	var enumValue = new QLrt.SimpleFormElementWidget({ name: "enum value", label: "Enum value - Quality:", valueWidget: new QLrt.EnumValueWidget(qualityEnum) }).appendTo(form);
+	var enumValue = new QLrt.SimpleFormElementWidget({ name: "enum value", label: "Enum value - Quality:", valueWidget: new QLrt.EnumValueWidget(qualityEnum) }).appendTo(valueSection);
 	new QLrt.SimpleFormElementWidget({ label: "enum choice defined?", valueWidget: new QLrt.BooleanValueWidget(
 			new QLrt.LazyValue(function () { return [ enumValue ]; }, function (enumValue) { return enumValue !== undefined; }, true)
-		) }).appendTo(form);
+		) }).appendTo(computedSection);
 	new QLrt.SimpleFormElementWidget({ label: "enum choice:", valueWidget: new QLrt.StringValueWidget(
 			new QLrt.LazyValue(function () { return [ enumValue ]; }, function (enumValue) { return enumValue; })
-		) }).appendTo(form);
+		) }).appendTo(computedSection);
 
-	var dateValue = new QLrt.SimpleFormElementWidget({ name: "date value", label: "Date value:", valueWidget: new QLrt.DateValueWidget() }).appendTo(form);
+	var dateValue = new QLrt.SimpleFormElementWidget({ name: "date value", label: "Date value:", valueWidget: new QLrt.DateValueWidget() }).appendTo(valueSection);
 	new QLrt.SimpleFormElementWidget({ label: "date value defined?", valueWidget: new QLrt.BooleanValueWidget(
 			new QLrt.LazyValue(function () { return [ dateValue ]; }, function (dateValue) { return dateValue !== undefined; }, true)
-		) }).appendTo(form);
+		) }).appendTo(computedSection);
 
-	var integerValue = new QLrt.SimpleFormElementWidget({ name: "integer value", label: "Integer value:", valueWidget: new QLrt.IntegerValueWidget() }).appendTo(form);
+	var integerValue = new QLrt.SimpleFormElementWidget({ name: "integer value", label: "Integer value:", valueWidget: new QLrt.IntegerValueWidget() }).appendTo(valueSection);
 	new QLrt.SimpleFormElementWidget({ label: "integer value defined?", valueWidget: new QLrt.BooleanValueWidget(
 			new QLrt.LazyValue(function () { return [ integerValue ]; }, function (integerValue) { return integerValue !== undefined; }, true)
-		) }).appendTo(form);
+		) }).appendTo(computedSection);
 
-	var decimalValue = new QLrt.SimpleFormElementWidget({ name: "decimal value", label: "Decimal value:", valueWidget: new QLrt.DecimalValueWidget() }).appendTo(form);
+	var decimalValue = new QLrt.SimpleFormElementWidget({ name: "decimal value", label: "Decimal value:", valueWidget: new QLrt.DecimalValueWidget() }).appendTo(valueSection);
 	new QLrt.SimpleFormElementWidget({ label: "decimal value defined?", valueWidget: new QLrt.BooleanValueWidget(
 			new QLrt.LazyValue(function () { return [ decimalValue ]; }, function (decimalValue) { return decimalValue !== undefined; }, true)
-		) }).appendTo(form);
+		) }).appendTo(computedSection);
 
-	var integerRangeValue = new QLrt.SimpleFormElementWidget({ name: "integer range value", label: "Integer range value:", valueWidget: new QLrt.RangeValueWidget({ min: 0, max: 10, type: 'integer' }) }).appendTo(form);
+	var integerRangeValue = new QLrt.SimpleFormElementWidget({ name: "integer range value", label: "Integer range value:", valueWidget: new QLrt.RangeValueWidget({ min: 0, max: 10, type: 'integer' }) }).appendTo(valueSection);
 	new QLrt.SimpleFormElementWidget({ label: "integer range value", valueWidget: new QLrt.IntegerValueWidget(
 			new QLrt.LazyValue(function () { return [ integerRangeValue ]; }, function (integerValue) { return integerValue; }, true)
-		) }).appendTo(form);
+		) }).appendTo(computedSection);
 
-	var booleanValue = new QLrt.SimpleFormElementWidget({ name: "boolean value", label: "Boolean value:", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(form);
+	var booleanValue = new QLrt.SimpleFormElementWidget({ name: "boolean value", label: "Boolean value:", valueWidget: new QLrt.BooleanValueWidget() }).appendTo(valueSection);
 	new QLrt.SimpleFormElementWidget({ label: "boolean value defined?", valueWidget: new QLrt.BooleanValueWidget(
 			new QLrt.LazyValue(function () { return [ booleanValue ]; }, function (booleanValue) { return booleanValue !== undefined; }, true)
-		) }).appendTo(form);
+		) }).appendTo(computedSection);
 
 	$('#QL-content').append(form.domElement());
 	form.activate();
--- reference-implementation/web/js/framework/structural-widgets.js
+++ reference-implementation/web/js/framework/structural-widgets.js
@@ -223,3 +223,76 @@ QLrt.ConditionalFormElementWidget = function (settings) {
 
 };
 QLrt.ConditionalFormElementWidget.prototype = Object.create(QLrt.Child.prototype);
+
+/** Section widget, contributed by Enrico Persiani (Whole Platform). */
+QLrt.SectionWidget = function (settings) {
+
+	QLrt.Child.call(this);
+
+	if (typeof (settings) !== 'object' || settings.label === undefined) {
+        throw 'invalid or incomplete settings';
+    }
+
+	var container = QLrt.mk('section').append(QLrt.mk('h1').text(settings.label));
+
+	this.domElement = function () {
+        return container;
+    };
+
+    var children = [];
+
+    this.append = function (widget) {
+        children.push(widget);
+        container.append(widget.domElement());
+    };
+
+    this.update = function () {
+        _.each(children, function (subWidget) {
+            subWidget.update();
+        });
+    };
+
+    this.defined = function () {
+        return _.all(children, function (subWidget) {
+            return subWidget.defined();
+        });
+    };
+
+    this.asJSON = function () {
+        var result = {};
+        _.each(children, function (subWidget) {
+            _.extend(result, subWidget.asJSON());
+        });
+        return result;
+    };
+
+};
+QLrt.SectionWidget.prototype = Object.create(QLrt.Child.prototype);
+
+/** Text widget, contributed by Enrico Persiani (Whole Platform). */
+QLrt.TextWidget = function (settings) {
+
+	QLrt.Child.call(this);
+
+	if (typeof (settings) !== 'object' || settings.text === undefined) {
+        throw 'invalid or incomplete settings';
+    }
+
+	var container = QLrt.mk('p').text(settings.text);
+
+	this.domElement = function () {
+        return container;
+    };
+
+    this.update = function () {};
+
+    this.defined = function () {};
+
+    this.asJSON = function () {
+        var result = {};
+        return result;
+    };
+
+};
+QLrt.TextWidget.prototype = Object.create(QLrt.Child.prototype);
+
